b9b15ddfcc097cb219a760f91488245211cb1b11
Gonzalo Perez de la Ossa <gonzalo@12tonelab.com>
2022-05-28 13:13:50 +0200

    Adds homing shots (initially only for Enemies)

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -9,22 +9,74 @@ public class Shot : MonoBehaviour
     [Header("Cfg")]
     [SerializeField] private float speed = 20f;
     [SerializeField] private bool isEnemyShot = false;
+    [Header("Homing (ENEMY ONLY)")]
+    [SerializeField] private bool isHoming = false;
+    [SerializeField] private float maxHorizontalSpeed = 3f;
+    [SerializeField] private float horizontalAcceleration = 2f;
 
 
     // Properties
     public bool IsEnemyShot { get { return isEnemyShot; } }
 
 
+    // Private
+    private Transform target;
+    private float horizontalSpeed = 0;
+
+
     #region Monobehaviour
 
-    private void Awake() {
-        Destroy(gameObject, 10f);
+    private void Awake()
+    {
+        Destroy(gameObject, 10f); // FUTURE: this is a quick and dirty fix, but would fail if a shot can last more than 10s
+    }
+
+
+    private void Start()
+    {
+        if (isHoming)
+        {
+            if (isEnemyShot)
+            {
+                target = FindObjectOfType<Ghost>().transform;
+            }
+        }
     }
 
 
     private void Update() {
-        transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        if (isHoming && target != null)
+        {
+            if (isEnemyShot)
+            {
+                horizontalSpeed = Mathf.MoveTowards(horizontalSpeed, maxHorizontalSpeed * DirectionToTarget(), horizontalAcceleration * Time.deltaTime);
+                Vector3 movement = Vector3.back * speed + Vector3.right * horizontalSpeed;
+                transform.position = transform.position + movement * Time.deltaTime;
+            }
+            else
+            {
+                 // TODO: homing Player shots. For now, just move them as if not homing
+                transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+        }
     }
 
+    #endregion
+
+
+
+    #region Private
+    
+    private int DirectionToTarget()
+    {
+        const float BUFFER = 0.2f;
+        if (transform.position.x < target.position.x - BUFFER) return 1;
+        else if (transform.position.x > target.position.x + BUFFER) return -1;
+        else return 0;
+    }
     #endregion
 }
